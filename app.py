e823da12b2532ac2aade4efeabd0916b12468acf
avycado13 <108358183+avycado13@users.noreply.github.com>
2023-12-26 11:13:51 +0530

    redo stuff

--- app.py
+++ app.py
@@ -1,70 +1,125 @@
-from flask import Flask, render_template, request, session
-from flask_socketio import SocketIO, emit
-import random
+from flask import Flask, render_template, request
+from flask_socketio import SocketIO, emit, join_room, leave_room
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
 socketio = SocketIO(app)
 
-class Game:
-    def __init__(self):
-        self.players = []
-        self.clues = []
-        self.code_words = []
-        self.current_turn = 0
-
-    def add_player(self, player_name):
-        self.players.append(player_name)
-
-    def generate_clues(self):
-        # Generate random clues for each player
-        self.clues = [str(random.randint(1000, 9999)) for _ in range(len(self.players))]
-
-    def generate_code_words(self):
-        # Generate random code words
-        words = ['apple', 'banana', 'cherry', 'date', 'elderberry']
-        self.code_words = random.sample(words, 4)
-
-    def update_game_state(self):
-        self.current_turn = (self.current_turn + 1) % len(self.players)
-
-game = Game()
+# Game state variables
+teams = {
+    'team1': {
+        'secret_words': ['apple', 'banana', 'cherry', 'date'],
+        'clue': '',
+        'clue_count': 0,
+        'guess': '',
+        'score': 0
+    },
+    'team2': {
+        'secret_words': ['elephant', 'giraffe', 'hippo', 'jaguar'],
+        'clue': '',
+        'clue_count': 0,
+        'guess': '',
+        'score': 0
+    }
+}
 
 @app.route('/')
-def home():
-    return render_template('home.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/new_game', methods=['POST'])
-def new_game():
-    game.add_player(request.form.get('player_name'))
-    return render_template('game.html', game=game)
-
-@app.route('/join_game', methods=['POST'])
-def join_game():
-    game.add_player(request.form.get('player_name'))
-    return render_template('game.html', game=game)
-
-@app.route('/start_game', methods=['POST'])
-def start_game():
-    game.generate_clues()
-    game.generate_code_words()
-    return render_template('game.html', game=game)
+@socketio.on('join')
+def handle_join(data):
+    team = data['team']
+    join_room(team)
+    emit('join_success', {'team': team})
 
 @socketio.on('submit_clue')
-def submit_clue(clue):
-    game.clues[game.current_turn] = clue
-    game.update_game_state()
-    emit('game_state', {'clues': game.clues, 'current_turn': game.current_turn}, broadcast=True)
+def handle_submit_clue(data):
+    team = data['team']
+    clue = data['clue']
+    clue_count = data['clue_count']
+    
+    # Validate clue and clue count
+    if not validate_clue(clue):
+        emit('clue_error', {'message': 'Invalid clue'})
+        return
+    
+    if not validate_clue_count(clue_count):
+        emit('clue_error', {'message': 'Invalid clue count'})
+        return
+    
+    teams[team]['clue'] = clue
+    teams[team]['clue_count'] = clue_count
+    emit('clue_submitted', {'team': team}, room=team)
 
-@socketio.on('guess_code_word')
-def guess_code_word(guess):
-    correct = guess == game.code_words[game.current_turn]
-    game.update_game_state()
-    emit('game_state', {'clues': game.clues, 'current_turn': game.current_turn, 'correct': correct}, broadcast=True)
+@socketio.on('submit_guess')
+def handle_submit_guess(data):
+    team = data['team']
+    guess = data['guess']
+    
+    # Validate guess
+    if not validate_guess(guess):
+        emit('guess_error', {'message': 'Invalid guess'})
+        return
+    
+    teams[team]['guess'] = guess
+    emit('guess_submitted', {'team': team}, room=team)
+    
+    # Check if both teams have submitted their guesses
+    if teams['team1']['guess'] and teams['team2']['guess']:
+        # Update game state and calculate scores
+        update_game_state()
+        emit('game_state_updated', {'teams': teams}, broadcast=True)
 
-@socketio.on('chat_message')
-def chat_message(message):
-    emit('chat_message', message, broadcast=True)
+def validate_clue(clue):
+    # Add your validation logic here
+    if len(clue) < 3:
+        return False
+    return True
+
+def validate_clue_count(clue_count):
+    # Add your validation logic here
+    if not isinstance(clue_count, int) or clue_count < 1 or clue_count > 4:
+        return False
+    return True
+
+def validate_guess(guess):
+    # Add your validation logic here
+    if len(guess) < 3:
+        return False
+    return True
+
+def update_game_state():
+    # Add your game logic here
+    # Update the scores, check for correct guesses, etc.
+    for team in teams.values():
+        secret_words = team['secret_words']
+        guess = team['guess']
+        score = 0
+
+        # Check if the guess matches any of the secret words
+        for word in secret_words:
+            if word == guess:
+                score += 1
+
+        # Update the team's score
+        team['score'] += score
+
+        # Reset the guess for the next round
+        team['guess'] = ''
+
+        # Add any additional game logic here
+
+        # Emit an event to update the team's score on the client-side
+        emit('score_updated', {'team': team, 'score': team['score']}, room=team)
+
+        # Reset the game state for the next round
+        team['clue'] = ''
+        team['clue_count'] = 0
+
+    # Add any additional game logic here
+
+    # Emit an event to update the game state on the client-side
+    emit('game_state_updated', {'teams': teams}, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app)
